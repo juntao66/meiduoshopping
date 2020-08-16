f37d0c512c4a509a24bb6f5137eb186c75c68bbb
ubuntu_tao <1401180436@qq.com>
2020-08-16 11:00:36 +0800

    create index

--- meiduoshop/meiduoshop/apps/users/views.py
+++ meiduoshop/meiduoshop/apps/users/views.py
@@ -1,8 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views import View
 from django import http
 from django.db import DatabaseError
 from users.models import User
+from django.urls import reverse
+from django.contrib.auth import login
 
 # Create your views here.
 class RegisterView(View):
@@ -13,12 +15,16 @@ class RegisterView(View):
         username = request.POST.get('username')
         password = request.POST.get('password')
         password2 = request.POST.get('password2')
+        mobile= request.POST.get('mobile')
 
-        if not all([username, password, password2]):
+
+        if not all([username, password]):
             return http.HttpResponseForbidden('not')
 
         try:
-            User.objects.create_user(username=username, password=password)
+           user = User.objects.create_user(username=username, password=password, mobile=mobile)
         except DatabaseError:
-            return render(request, 'register.html', {'register_errmsg':'failure'})
-        return http.HttpResponse('sucess')
+            return http.HttpResponse('database error')
+        
+        login(request,  user)
+        return redirect(reverse('contents:index'))
